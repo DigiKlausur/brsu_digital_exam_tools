df28242d81d062fc30781e013e64e1206702f8ff
Mohammad Wasil <mwasil@outlook.co.id>
2019-07-22 18:10:58 +0200

    add author

--- create_multiple_users/remove_bulk_users.bash
+++ create_multiple_users/remove_bulk_users.bash
@@ -1,3 +1,8 @@
+#title           :remove_bulk_users.sh
+#description     :This script will remove multi Linux users
+#author		       :Mohammad Wasil
+#Copyright 2019, DigiKlausur project, Bonn-Rhein-Sieg University
+#==============================================================================
 for username in $(< dellist.txt)
   do
     echo "$username"
